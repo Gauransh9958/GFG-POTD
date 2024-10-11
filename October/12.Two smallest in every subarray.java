0df6b8a72b4692e801eb11360950eb92aabb3813
Gauransh9958 <gauransh9958@gmail.com>
2024-10-12 00:04:30 +0530

    changed

---
+++ October/12.Two smallest in every subarray.java
@@ -0,0 +1,18 @@
+
+
+class Solution {
+    public int pairWithMaxSum(int[] arr) {
+        if (arr.length < 2) {
+            return -1;
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int currentSum = arr[i] + arr[i + 1];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
