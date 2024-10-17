b29fd33d72d866575afb7c0a7890a5200ee9daba
Gauransh9958 <gauransh9958@gmail.com>
2024-10-17 08:14:55 +0530

    changed

---
+++ October/Split Linked List Alternatingly.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Solution:
+    def alternatingSplitList(self, head):
+        if head is None:
+            return [None, None]
+
+        head1, head2 = None, None
+        tail1, tail2 = None, None
+        add_to_first = True
+        current = head
+        while current:
+            if add_to_first:
+                if head1 is None:
+                    head1 = tail1 = current
+                else:
+                    tail1.next = current
+                    tail1 = tail1.next
+            else:
+                if head2 is None:
+                    head2 = tail2 = current
+                else:
+                    tail2.next = current
+                    tail2 = tail2.next
+
+            add_to_first = not add_to_first
+            current = current.next
+
+        if tail1:
+            tail1.next = None
+        if tail2:
+            tail2.next = None
+
+        return [head1, head2]
