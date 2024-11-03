bde958c9141a0cbffba3d763ce5922d34a5f0d7d
Gauransh9958 <gauransh9958@gmail.com>
2024-11-03 11:34:17 +0530

    changed

---
+++ November/3.Is Linked List Length Even.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isLengthEven(self, head):
+        is_even = True  
+        
+        current = head
+        
+        while current:
+            is_even = not is_even
+            current = current.next
+        
+        return is_even
