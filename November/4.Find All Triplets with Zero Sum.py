9752dd3f155e128996d7940b47d6f4f9854d553c
Gauransh9958 <gauransh9958@gmail.com>
2024-11-04 12:59:05 +0530

    changed

---
+++ November/4.Find All Triplets with Zero Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTriplets(self, arr):
+        res = []
+        n = len(arr)
+    
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                for k in range(j + 1, n):
+                
+                    if arr[i] + arr[j] + arr[k] == 0:
+                        res.append([i, j, k])
+        return res
+    
