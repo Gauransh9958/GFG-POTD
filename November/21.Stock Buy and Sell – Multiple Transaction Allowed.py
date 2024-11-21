9c50d085fb76609f41bb0c5c072b8f3c3a5c3539
Gauransh9958 <gauransh9958@gmail.com>
2024-11-21 19:47:16 +0530

    changed

---
+++ November/21.Stock Buy and Sell – Multiple Transaction Allowed.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def maximumProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                maxProfit += prices[i] - prices[i - 1]
+        return maxProfit
+
+
