88c180b024bf4f86678340f7f19b7b8f8831459e
Gauransh9958 <gauransh9958@gmail.com>
2025-02-07 20:36:31 +0530

    changed

---
+++ February/7.Inorder Traversal.py
@@ -0,0 +1,34 @@
+
+
+
+class Solution:
+
+    def inOrder(self, root):
+        res = []
+        curr = root
+
+        while curr is not None:
+            if curr.left is None:
+
+
+                res.append(curr.data)
+                curr = curr.right
+            else:
+
+                prev = curr.left
+                while prev.right is not None \
+                and prev.right != curr:
+                    prev = prev.right
+
+
+                if prev.right is None:
+                    prev.right = curr
+                    curr = curr.left
+                else:
+
+
+                    prev.right = None
+                    res.append(curr.data)
+                    curr = curr.right
+
+        return res
