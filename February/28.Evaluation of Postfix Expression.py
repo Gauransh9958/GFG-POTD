b8891a61965c2d103fa43c73c8faa841f9d0daa4
Gauransh9958 <gauransh9958@gmail.com>
2025-02-28 21:07:30 +0530

    changed

---
+++ February/28.Evaluation of Postfix Expression.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def evaluate(self, arr):
+        stack = []
+        for token in arr:
+            if token[-1].isdigit():
+                stack.append(int(token))
+            else:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if token == "+":
+                    stack.append(num1 + num2)
+                elif token == "-":
+                    stack.append(num1 - num2)
+                elif token == "*":
+                    stack.append(num1 * num2)
+                elif token == "/":
+                    stack.append(int(num1 / num2))
+        return stack[-1]
