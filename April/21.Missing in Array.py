4ea9aa7df3e652b42ca58fb0e33762c8def89201
Gauransh9958 <gauransh9958@gmail.com>
2025-04-21 16:34:42 +0530

    changed

---
+++ April/21.Missing in Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def missingNum(self, arr):
+        n = len(arr) + 1
+        xor1 = 0
+        xor2 = 0
+
+        for num in arr:
+            xor2 ^= num
+
+        for i in range(1, n + 1):
+            xor1 ^= i
+
+        return xor1 ^ xor2
