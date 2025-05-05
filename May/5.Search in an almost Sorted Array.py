4fe475b41cf9d1530051c13e2e053e78e465b95d
Gauransh9958 <gauransh9958@gmail.com>
2025-05-05 20:10:07 +0530

    changd

---
+++ May/5.Search in an almost Sorted Array.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def findTarget(self, arr, target):
+        l, r = 0, len(arr) - 1
+
+        while r >= l:
+            mid = l + (r - l) // 2
+
+            if arr[mid] == target:
+                return mid
+            if mid > l and arr[mid - 1] == target:
+                return mid - 1
+            if mid < r and arr[mid + 1] == target:
+                return mid + 1
+
+            if arr[mid] > target:
+                r = mid - 2
+
+            
+            else:
+        
+        
+                l = mid + 2
+
+        return -1
